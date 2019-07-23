82d6c79a28941a3da973d8621751333014bd2596
Sam Tricamo <samualtricamo@gmail.com>
2019-07-23 15:41:20 -0500

    firebase added

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+document.ready(function() {
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCPKj4DmerLEbeMO2YOCnVtbNUs3GLfPow",
+    authDomain: "activity-11-3d572.firebaseapp.com",
+    databaseURL: "https://activity-11-3d572.firebaseio.com",
+    projectId: "activity-11-3d572",
+    storageBucket: "",
+    messagingSenderId: "372664582740",
+    appId: "1:372664582740:web:2d3ce8868717df5b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
