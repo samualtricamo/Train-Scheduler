e659cdba16ddafc497caf5053b66340c6e25bbd6
Sam Tricamo <samualtricamo@gmail.com>
2019-07-23 15:52:28 -0500

    creating clickability

--- assets/app.js
+++ assets/app.js
@@ -13,3 +13,26 @@ document.ready(function() {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  var dataRef = firebase.database();
+  $("#submit-btn").on("click", function(event) {
+      event.preventDefault();
+
+var name = $("#name").val().trim();
+var destination = $("#destination").val().trim();
+var firstTrain = $("#firstTrain").val().trim();
+var frequency = $("#frequency").val().trim();
+
+$("#name").val("");
+$("#destination").val("");
+$("#firstTrain").val("");
+$("#frequency").val("");
+
+dataRef.ref().push({
+    name: name,
+    destination: destination,
+    time: firstTrain,
+    frequency: frequency
+});
+  });
+
